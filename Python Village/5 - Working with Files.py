54d022cbc56c83c71dc465374bdf96a65283561a
dataducky <setoking55@gmail.com>
2024-05-26 11:53:26 -0500

    Completed Python Village problem 5

--- Python Village/5 - Working with Files.py
+++ Python Village/5 - Working with Files.py
@@ -1,3 +1,11 @@
-f = open('5inputfile.txt', 'r')
-for i in f:
-    print(i)
+counter = 0
+output = open('5outputfile.txt', 'w')
+with open('5inputfile.txt', 'r') as f:
+    for i in f:
+        counter += 1
+        if counter % 2 == 0:
+            output.write(i)
+output.close()
+output = open('5outputfile.txt', 'r')
+for line in output:
+    print(line.strip())
