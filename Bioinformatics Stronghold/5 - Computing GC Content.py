29813eeacd6349d9185b252d529630578395be67
dataducky <setoking55@gmail.com>
2024-05-28 01:39:05 -0500

    Completed BioStronghold 5

--- Bioinformatics Stronghold/5 - Computing GC Content.py
+++ Bioinformatics Stronghold/5 - Computing GC Content.py
@@ -1,3 +1,55 @@
-GCinput = open("5inputfile.txt","r")
+import re
+#initalize values
+GCdict = {}
+count, lineCount, lineTotal = 0, 0, 0
+match = r'^>'
+strID = ""
+strand = ""
+#counts total lines for last entry to be printed
+with open(r"5inputfile.txt", 'r') as totaler:
+    lineTotal = len(totaler.readlines())
+
+GCinput = open('5inputfile.txt','r')
 for line in GCinput:
-    print(line)
+    lineCount += 1
+    if re.match(match, line):
+        # GC % calculator, saves to dict assigned to strID
+        if strand != "":
+            GCPercentage = 0.0
+            GCcount, total = 0, 0
+            for i in strand.strip():
+                if i == "G" or i == "C":
+                    GCcount += 1
+                total += 1
+            GCPercentage = GCcount / total
+            GCdict[strID] = GCPercentage
+
+        #initializes strand ID to save to dict
+        strID = line[1:].strip() 
+        
+        # ending sequence 
+        count += 1
+        strand = ""
+    else:
+        strand += line.strip()
+    if lineCount == lineTotal:
+        GCPercentage = 0.0
+        GCcount, total = 0, 0
+        for i in strand:
+            if i == "G" or i == "C":
+                GCcount += 1
+            total += 1
+        GCPercentage = GCcount / total
+        GCdict[strID] = GCPercentage
+GCinput.close()
+
+#highest value-ator
+highestValue, highestKey = 0.0, ""
+for key, value in GCdict.items():
+    print(key + " | " + str(value))
+    if value >= highestValue:
+        highestValue = value
+        highestKey = key
+print (highestKey)
+print (highestValue * 100)
+print(GCdict)
