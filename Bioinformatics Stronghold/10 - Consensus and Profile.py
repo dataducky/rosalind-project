4397c6502929a4145f00a2c25aacd8469ffbc0a6
dataducky <setoking55@gmail.com>
2024-09-20 21:47:48 -0500

    Completed Problem 10
    -updated 10inputfile with new data set
    -updated Problem 10 file with the following changes:
    *removed debug code
    ~altered var strandLength to use dict keys
    +added consensus string & print portion

--- Bioinformatics Stronghold/10 - Consensus and Profile.py
+++ Bioinformatics Stronghold/10 - Consensus and Profile.py
@@ -34,11 +34,12 @@ GCinput.close()
 
 #---profile matrix portion---
 #i is the strID, j is the strand.
-print(DNAdict)
-strandLength = len(DNAdict["Rosalind_1"]) - 1
+# create dict
+first_key = list(DNAdict.keys())[0]
+strandLength = len(DNAdict[first_key]) - 1
 strandList = []
 indexCount = 0
-for i in DNAdict["Rosalind_1"]:
+for i in DNAdict[first_key]:
     strandList.append(0)
 PROFDict = {"Astr":strandList, "Cstr":strandList,"Gstr":strandList,"Tstr":strandList}
 ProfileMatrix = pd.DataFrame(data=PROFDict)
@@ -60,4 +61,40 @@ for i in DNAdict:
             ProfileMatrix.loc[nucCount, "Tstr"] += 1
         nucCount += 1
 
-print(ProfileMatrix.T)
+nucCount = 0
+consensusString = ""
+while nucCount <= strandLength:
+    highestValue = 0
+    consensusNuc = ""
+    for i in ProfileMatrix:
+        if ProfileMatrix[i][nucCount] > highestValue:
+            highestValue = ProfileMatrix[i][nucCount]
+            consensusNuc = i
+    if consensusNuc == "Astr":
+        consensusString += ("A")
+    if consensusNuc == "Cstr":
+        consensusString += ("C")
+    if consensusNuc == "Gstr":
+        consensusString += ("G")
+    if consensusNuc == "Tstr":
+        consensusString += ("T")
+    nucCount += 1
+
+    
+print(consensusString)
+APrint = "A: "
+for i in ProfileMatrix.Astr:
+    APrint += str(i) + " "
+print(APrint)
+CPrint = "C: "
+for i in ProfileMatrix.Cstr:
+    CPrint += str(i) + " "
+print(CPrint)
+GPrint = "G: "
+for i in ProfileMatrix.Gstr:
+    GPrint += str(i) + " "
+print(GPrint)
+TPrint = "T: "
+for i in ProfileMatrix.Tstr:
+    TPrint += str(i) + " "
+print(TPrint)
